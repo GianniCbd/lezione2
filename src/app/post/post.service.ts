63458e70492908248d10b9cf63b5a48c0883daa9
GianniCbd <giannicabiddu@gmail.com>
2023-12-07 10:53:46 +0100

    try

--- src/app/post/post.service.ts
+++ src/app/post/post.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Post } from './post';
 
 @Injectable({
@@ -8,10 +8,21 @@ import { Post } from './post';
 })
 export class PostService {
   private apiUrl = 'assets/db.json';
+  private posts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(this.apiUrl);
+  }
+
+  updatePost(updatedPost: Post): Observable<void> {
+    const index = this.posts.findIndex((posts) => posts.id === updatedPost.id);
+
+    if (index !== -1) {
+      this.posts[index] = updatedPost;
+    }
+
+    return of(undefined);
   }
 }
